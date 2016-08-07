f07894895ce4eea60f398b03ece89716645d3ad6
anvimol <anvimol@gmail.com>
2016-08-07 13:13:36 +0200

    Archivos del curso de Laravel

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	<link rel="stylesheet" href="{{asset('css/web.css')}}">
+	<!-- <link rel="stylesheet" href="/laravel52/public/css/web.css"> -->
+</head>
+<body>
+
+	<div class="container">
+		<nav class="navbar navbar-default navbar-top">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			    </button>
+		    </div>
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		    	<ul class="nav navbar-nav">
+					<li><a class="active" href="#">Inicio</a></li>
+					<li><a href="#">Secciones</a></li>
+					<li><a href="#">Perfil</a></li>
+		    	</ul>
+		    </div>
+		  </div>
+		</nav>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="https://use.fontawesome.com/7d6837c833.js"></script>
+	<script src="{{asset('js/web.js')}}"></script>
+	<!-- <script src="/laravel52/public/js/web.js"></script> -->
+</body>
+</html>
